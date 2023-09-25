78229a1b0222ab8a18a3d29b45bc2057393478b6
dylanimperato1 <47250910+dylanimperato1@users.noreply.github.com>
2023-09-24 23:18:43 -0400

    push

---
+++ project2/Assets/Code/PlayerCode.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerCode : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        for (int i = 0; i < PublicVars.Instance.playerResources.Length; i++)
+        {
+            
+            if ((PublicVars.Instance.playerResources[i] >= PublicVars.Instance.resourceCap) || (PublicVars.Instance.playerResources[i] <= 0)) {
+                SceneManager.LoadScene("GameOver");
+            } 
+
+            if(PublicVars.Instance.playerResources[4] >= 300){
+                SceneManager.LoadScene("Win");
+            }
+
+        }
+    }
+}
